96303e46da63a183f80cf1c8f2c30d894c51642a
Shining <wtf@shininglovestar.com>
2023-04-17 18:53:42 +0200

    good stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ParallaxProvider } from "react-scroll-parallax"
 
-// import { Home } from "./routes/home/Home"
 import "./app.css"
 import ConsciousnessSessionLp from "./routes/consciousness-session-lp/ConsciousnessSessionLp"
 import logo from "./assets/logo.png"
 import Trippy from "./routes/trippy/Trippy"
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <Home />,
-  // },
   {
     path: "/",
     element: <Trippy />,
   },
-  // {
-  //   path: "/",
-  //   element: <ConsciousnessSessionLp />,
-  // },
-
   {
     path: "/lp",
     element: <ConsciousnessSessionLp />,
